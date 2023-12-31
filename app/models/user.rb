9fc9d9403f0f21d8fd514c4e91fcf080ff029382
EC2 Default User <0311.hnmyuya@gmail.com>
2023-12-31 04:21:10 +0000

    フォロー・フォロワー機能の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,33 @@ class User < ApplicationRecord
   has_many :books
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  has_one_attached :profile_image
+  has_many :follower_relationships, foreign_key: "followed_id", class_name: "Relationship", dependent: :destroy
+  has_many :followers, through: :follower_relationships, source: :follower
+  has_many :followed_relationships, foreign_key: "follower_id", class_name: "Relationship", dependent: :destroy
+  has_many :following, through: :followed_relationships, source: :followed
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
 
+  has_one_attached :profile_image
+  
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
   
   def get_profile_image
     profile_image.attached? ? profile_image : 'no_image.jpg'
+  end
+  
+  # ユーザーをフォローする
+  def follow(user)
+    relationships.create(followed_id: user.id)
+  end
+  
+  # フォローを外すとき
+  def unfollow(user)
+    relationships.find_by(followed_id: user.id).destroy
+  end
+
+  # フォローしているか確認するとき
+  def following?(user)
+    following.include?(user)
   end
 end
