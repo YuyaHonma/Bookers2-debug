274683aca29e6cb8cc997fb03636b1bab526f8bb
EC2 Default User <0311.hnmyuya@gmail.com>
2024-01-10 16:14:02 +0000

    chart.jsを使って過去7日分の投稿数をグラフ化する

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,43 +3,40 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @books = @user.books.page(params[:page]).per(10).order(created_at: :desc)
+    @books = @user.books
     @book = Book.new
     @currentUserEntry = Entry.where(user_id: current_user.id)
     @userEntry = Entry.where(user_id: @user.id)
     @isRoom = false
     @roomId = nil
+    
 
-    unless @user.id == current_user.id
-      @currentUserEntry.each do |cu| 
-        @userEntry.each do |u| 
-          if cu.room_id == u.room_id 
-            @isRoom = true
-            @roomId = cu.room_id
-          end
-        end
+unless @user.id == current_user.id
+  @currentUserEntry.each do |cu| 
+    @userEntry.each do |u| 
+      if cu.room_id == u.room_id 
+        @isRoom = true
+        @roomId = cu.room_id
       end
     end
+  end
+end
 
-    if @isRoom
-    else
-      @room = Room.new
-      @entry = Entry.new
-    end
+if @isRoom
+else
+  @room = Room.new
+  @entry = Entry.new
+end
 
-    @today_book = @books.created_today
-    @yesterday_book = @books.created_yesterday
-    @this_week_book = @books.created_this_week
-    @last_week_book = @books.created_last_week
   end
 
   def index
     @users = User.all
     @book = Book.new
   end
-
   def edit
     @user = User.find(params[:id])
+    
   end
 
   def update
